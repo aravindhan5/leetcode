7f259f5c24343693ca4eccc9cd2daa6741a842c0
aravindhan5 <aravindhanm369@gmail.com>
2026-07-20 09:28:15 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (18.09%) - LeetSync

---
+++ 66-plus-one/plus-one.py
@@ -0,0 +1,14 @@
+class Solution:
+    def plusOne(self, digits: list[int]) -> list[int]:
+        n = len(digits)
+        
+        # Traverse the array from right to left
+        for i in range(n - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+            
+        # If all digits were 9
+        return [1] + digits
+
